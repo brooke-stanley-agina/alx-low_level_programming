e0caa1a741c16231cd3920962cbe002b8631e969
Brooke Stanley Agina <a1234@1234deMacBook-Pro.local>
2022-03-15 15:17:26 +0300

    0-putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,9 +1,9 @@
-#include <main.h>
+#include "main.h"
 /**
  * main - prints the string "_putchar" from a character array.
  * Return: 0 on success.
  **/
 int main(void)
 {
-	_putchar('_putchar');	
+	_putchar('_putchar \n');
 }
